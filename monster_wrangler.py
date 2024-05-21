4db85eb6bede07589895b0533f441f3ed86b46d0
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-05-20 20:04:26 -0700

    monster.py created and tested

--- monster_wrangler.py
+++ monster_wrangler.py
@@ -1,5 +1,6 @@
 import pygame, random
 from player import Player
+from monster import Monster
 
 #Initialize pygame
 pygame.init()
@@ -221,36 +222,6 @@ class Game():
         self.player.reset()
 
         self.start_new_round()
-
-
-class Monster(pygame.sprite.Sprite):
-    """A class to create enemy monster objects"""
-    def __init__(self, x, y, image, monster_type):
-        """Initialize the monster"""
-        super().__init__()
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
-
-        #Monster type is an int 0 -> blue, 1 -> green, 2 -> purple, 3 -> yellow
-        self.type = monster_type
-
-        #Set random motion
-        self.dx = random.choice([-1, 1])
-        self.dy = random.choice([-1, 1])
-        self.velocity = random.randint(1, 5)
-
-
-    def update(self):
-        """Update the monster"""
-        self.rect.x += self.dx*self.velocity
-        self.rect.y += self.dy*self.velocity
-
-        #Bounce the monster off the edges of the display
-        if self.rect.left <= 0 or self.rect.right >= WINDOW_WIDTH:
-            self.dx = -1*self.dx
-        if self.rect.top <= 100 or self.rect.bottom >= WINDOW_HEIGHT - 100:
-            self.dy = -1*self.dy
 
 
 #Create a player group and Player object

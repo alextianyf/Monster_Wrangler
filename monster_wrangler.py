569d766f1d24a6a81cdc0f6e63394ec6685d57e4
Alex Tian <70412334+alextianyf@users.noreply.github.com>
2024-05-20 20:01:56 -0700

    player.py created and tested

--- monster_wrangler.py
+++ monster_wrangler.py
@@ -1,4 +1,5 @@
 import pygame, random
+from player import Player
 
 #Initialize pygame
 pygame.init()
@@ -220,54 +221,6 @@ class Game():
         self.player.reset()
 
         self.start_new_round()
-
-
-class Player(pygame.sprite.Sprite):
-    """A player class that the user can control"""
-    def __init__(self):
-        """Initialize the player"""
-        super().__init__()
-        self.image = pygame.image.load("knight.png")
-        self.rect = self.image.get_rect()
-        self.rect.centerx = WINDOW_WIDTH//2
-        self.rect.bottom = WINDOW_HEIGHT
-
-        self.lives = 5
-        self.warps = 2
-        self.velocity = 8
-
-        self.catch_sound = pygame.mixer.Sound("catch.wav")
-        self.die_sound = pygame.mixer.Sound("die.wav")
-        self.warp_sound = pygame.mixer.Sound("warp.wav")
-
-
-    def update(self):
-        """Update the player"""
-        keys = pygame.key.get_pressed()
-
-        #Move the player within the bounds of the screen
-        if keys[pygame.K_LEFT] and self.rect.left > 0:
-            self.rect.x -= self.velocity
-        if keys[pygame.K_RIGHT] and self.rect.right < WINDOW_WIDTH:
-            self.rect.x += self.velocity
-        if keys[pygame.K_UP] and self.rect.top > 100:
-            self.rect.y -= self.velocity
-        if keys[pygame.K_DOWN] and self.rect.bottom < WINDOW_HEIGHT - 100:
-            self.rect.y += self.velocity
-
-
-    def warp(self):
-        """Warp the player to the bottom 'safe zone'"""
-        if self.warps > 0:
-            self.warps -= 1
-            self.warp_sound.play()
-            self.rect.bottom = WINDOW_HEIGHT
-
-
-    def reset(self):
-        """Resets the players position"""
-        self.rect.centerx = WINDOW_WIDTH//2
-        self.rect.bottom = WINDOW_HEIGHT
 
 
 class Monster(pygame.sprite.Sprite):
